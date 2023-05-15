933432d7cdc90e2715e2cf61dce3aeb65570ca9f
Lautaro Chaparro <lautaro.chaparro.et21.21@gmail.com>
2023-05-15 01:30:02 -0300

    primer despegue 6

--- web_kodak_django/settings.py
+++ web_kodak_django/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = 'static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'),)
-    STATICFILES_STORAGE = 'whitenoise.django.GzipManifestSatoticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
